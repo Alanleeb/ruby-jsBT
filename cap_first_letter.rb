eb74dad7ff0c4439f154291f25018dce21dcec88
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-23 17:13:46 -0600

    capitalize the first letter of each word

---
+++ cap_first_letter.rb
@@ -0,0 +1,20 @@
+def capitalize ()
+    puts "enter a sentence"
+    sentence = gets.chomp
+    words = sentence.split(' ')
+
+    words.each_with_index do |word, index|
+        letters = word.split('')
+
+        letters[0] = letters[0].upcase
+        word = letters.join('')
+        words[index] = word
+         #puts word
+    end
+
+    sentence = words.join(' ')
+    return sentence
+
+    end
+
+puts capitalize()
