52a4fab662f2c2a0e56dbfec085f9ecc79f2c167
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-27 18:34:33 -0600

    working loop

--- word_combos.rb
+++ word_combos.rb
@@ -1,9 +1,24 @@
-def word_combos
-  puts 'please enter a word'
-  word = [] << gets.chomp.split("")
+def word_combos(word)
+  if word.length == 0
+   exit
+  end
   i = 0
+  o = 0
+    while i < word.length
+      print (word[o..i])
+      print (", ")
+      i+=1
+    end
+    x = word.split("")
+    x.shift
+    word = x.join("")
+   word_combos(word)
+  end
 
-  word.each{|f| puts f[i] }
+def word
+  puts 'please enter a word'
+  word = gets.chomp.strip
+  word_combos(word)
 end
 
-word_combos
+word
